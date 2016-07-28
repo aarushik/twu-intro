0de96dde187464af799abeaae5b72897eb4a0383
Aarushi Kansal <aarushi.k@hotmail.com>
2016-07-28 20:33:26 +1000

    Asking for user input and drawing a horizontal line of stars

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.print("*");
+
+        //System.out.print("*");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numOfStars = 0;
+
+        System.out.println("Enter an integer");
+        try {
+            numOfStars = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while (numOfStars != 0) {
+            System.out.print("*");
+            numOfStars--;
+        }
+
     }
 }
