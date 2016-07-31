90112ecdb5f5af9d828c423bff7eca39edab6310
Aarushi Kansal <aarushi.k@hotmail.com>
2016-08-01 01:46:52 +1000

    Added fizz buzz, prime factors, diamonds exercises

---
+++ src/Diamond.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Aarushi on 30/07/2016.
+ */
+public class Diamond {
+}
---
+++ src/Triangles.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Aarushi on 29/07/2016.
+ */
+public class Triangles {
+}
